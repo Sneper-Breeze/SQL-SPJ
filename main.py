b9ed4e09946450a147dd66cd4eb2e44cd05ee361
Maksim <billsifer00@gmail.com>
2024-06-21 12:30:56 +0000

    Improved main

--- main.py
+++ main.py
@@ -1,68 +1,63 @@
-from antlr4 import *
+import antlr4
 from parser.sqlLexer import sqlLexer
 from parser.sqlParser import sqlParser
 from spj import SQLLogic
-from nodes import *
+from nodes import AbstractNode
 import sys
 
 
-
-def print_bin_tree(node : AbstractNode, ind):
+def print_bin_tree(node: AbstractNode, ind):
     print('--' * ind + str(node))
 
-    if(node.give_LChind() != None):
+    if (node.give_LChind() is not None):
         print_bin_tree(node.give_LChind(), ind+1)
-    if(node.give_RChind() != None):
+    if (node.give_RChind() is not None):
         print_bin_tree(node.give_RChind(), ind+1)
 
 
 def test(argv):
     print('TEST:')
+    lexer = sqlLexer(antlr4.FileStream('test/input.txt'))
+    stream = antlr4.CommonTokenStream(lexer)
+    parser = sqlParser(stream)
+    tree = parser.query()
 
-    joinNodeUp = JoinNode('Stats.item', 'Sellings.item')
-    joinNodeBot = JoinNode('Sellings.userId', 'Users.id')
+    print(tree.toStringTree(recog=parser))
 
-    rtable_node = TableNode('Users')
-    ltable_node = TableNode('Sellings')
-    rrtable_node = TableNode('Stats')
-
-    joinNodeBot.set_LChild(ltable_node)
-    joinNodeBot.set_RChild(rtable_node)
-
-    joinNodeUp.set_LChild(joinNodeBot)
-    joinNodeUp.set_RChild(rrtable_node)
-
-    whereNode = WhereNode('Stats.cost', 'Stats.damage', list('>'))
-    whereNode.set_LChild(joinNodeUp)
-
-    selectNode = SelectNode(['Stats.item', 'Sellings.item', 'Sellings.userId', 'Users.id', 'Stats.cost', 'Stats.damage'])
-    selectNode.set_LChild(whereNode)
-    selectNode.open()
-    print(*[selectNode.get_next() for _ in range(4)])
-
-    selectNode.close()
+    listener = SQLLogic()
+    antlr4.ParseTreeWalker().walk(listener, tree)
+    root = listener.getRoot()
+    print_bin_tree(root, 0)
+    root.open()
 
 
 def main(argv):
-    # input_text = input('> ')
-    # lexer = sqlLexer(InputStream(input_text))
     if '--test' in argv:
         test(argv)
         return
 
-    lexer = sqlLexer(FileStream('test/input.txt'))
-    stream = CommonTokenStream(lexer)
+    # input_text = input('> ')
+    # lexer = sqlLexer(InputStream(input_text))
+    lexer = sqlLexer(antlr4.FileStream('test/input.txt'))
+    stream = antlr4.CommonTokenStream(lexer)
     parser = sqlParser(stream)
     tree = parser.query()
 
-    # print(tree.toStringTree(recog=parser))
-
     listener = SQLLogic()
-    ParseTreeWalker().walk(listener, tree)
+    antlr4.ParseTreeWalker().walk(listener, tree)
     root = listener.getRoot()
-    print_bin_tree(root, 0)
+
     root.open()
-    print(root.get_next())
+
+    data_block = root.get_next()
+    res = []
+    while (data_block is not None):
+        res += data_block
+        data_block = root.get_next()
+    print(*res, sep='\n')
+
+    root.close()
+
 
 if __name__ == '__main__':
     main(sys.argv)
